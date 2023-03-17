5a567db72f2775b196e247b4ed1dbebeca0ca501
twillsonepitech <t.willson@outlook.fr>
2023-03-18 00:18:46 +0100

    mod: get next line

--- get_next_line.c
+++ get_next_line.c
@@ -22,19 +22,16 @@
 void free_args_va_arguments(const int size_args, ...)
 {
     va_list ap;
-    int __i = ZERO;
+    int i = ZERO;
     void *ptr;
 
     va_start(ap, size_args);
-    while (__i < size_args)
-    {
+    while (i < size_args) {
         ptr = va_arg(ap, void *);
         if (NULL == ptr)
-        {
             break;
-        }
         free(ptr);
-        __i++;
+        i++;
     }
     va_end(ap);
 }
@@ -48,22 +45,19 @@ int join(char **__restrict __dest, const char *__restrict __src)
     int number_of_index_fill;
 
     tempr = (char *) malloc(sizeof(char) * (source_size + dest_size + 1));
-    if (NULL == tempr)
-    {
+    if (NULL == tempr) {
         PUT_ERROR(strerror(errno));
         return FUNCTION_FAILURE;
     }
     number_of_index_fill = INIT_INTEGER;
     index_per_iteration = INIT_INTEGER;
-    while (index_per_iteration < (int)(dest_size))
-    {
+    while (index_per_iteration < (int)(dest_size)) {
         tempr[number_of_index_fill] = (*__dest)[index_per_iteration];
         number_of_index_fill++;
         index_per_iteration++;
     }
     index_per_iteration = INIT_INTEGER;
-    while (index_per_iteration < (int)(source_size))
-    {
+    while (index_per_iteration < (int)(source_size)) {
         tempr[number_of_index_fill] = __src[index_per_iteration];
         number_of_index_fill++;
         index_per_iteration++;
@@ -79,8 +73,7 @@ int number_iteration_until_end(const char *__restrict lineptr, size_t index_star
     int index_per_iteration;
 
     index_per_iteration = INIT_INTEGER;
-    while (lineptr[index_start_lineptr] != end)
-    {
+    while (lineptr[index_start_lineptr] != end) {
         index_start_lineptr++;
         index_per_iteration++;
     }
@@ -92,14 +85,12 @@ int init_buffer(char **__restrict __dest, const char *__restrict __src, size_t i
     int index_per_iteration;
 
     *__dest = (char *)malloc(sizeof(char) * (number_iteration_until_end(__src, index_start_src, end) + 1));
-    if (NULL == *__dest)
-    {
+    if (NULL == *__dest) {
         PUT_ERROR(strerror(errno));
         return FUNCTION_FAILURE;
     }
     index_per_iteration = INIT_INTEGER;
-    while (__src[index_start_src] != end)
-    {
+    while (__src[index_start_src] != end) {
         (*__dest)[index_per_iteration] = __src[index_start_src];
         index_start_src++;
         index_per_iteration++;
@@ -115,24 +106,17 @@ int join_return_value(char *__restrict left_buffer, char *__restrict right_buffe
 
     return_from_function = join(ptr_return, *ptr_container);
     if (FUNCTION_FAILURE == return_from_function)
-    {
         return FUNCTION_FAILURE;
-    }
     return_from_function = join(ptr_return, left_buffer);
     if (FUNCTION_FAILURE == return_from_function)
-    {
         return FUNCTION_FAILURE;
-    }
-    if (*ptr_container != NULL)
-    {
+    if (*ptr_container != NULL) {
         free(*ptr_container);
         *ptr_container = NULL;
     }
     return_from_function = join(ptr_container, right_buffer);
     if (FUNCTION_FAILURE == return_from_function)
-    {
         return FUNCTION_FAILURE;
-    }
     return FUNCTION_SUCCESS;
 }
 
@@ -144,21 +128,16 @@ int manage_buffer(char *__restrict buffer, char **__restrict ptr_container, char
 
     return_from_function = init_buffer(&left_buffer, buffer, INIT_INTEGER, EOL);
     if (FUNCTION_FAILURE == return_from_function)
-    {
         return FUNCTION_FAILURE;
-    }
     return_from_function = init_buffer(&right_buffer, buffer, strlen(left_buffer), NIL);
-    if (FUNCTION_FAILURE == return_from_function)
-    {
+    if (FUNCTION_FAILURE == return_from_function) {
         free(left_buffer);
         return FUNCTION_FAILURE;
     }
     return_from_function = join_return_value(left_buffer, right_buffer, ptr_container, ptr_return);
     free_args_va_arguments(TWO, left_buffer, right_buffer);
     if (FUNCTION_FAILURE == return_from_function)
-    {
         return FUNCTION_FAILURE;
-    }
     return FUNCTION_SUCCESS;
 }
 
@@ -168,70 +147,50 @@ int read_line_into_buffer(const int fd, char **__restrict ptr_container, char **
     ssize_t readline;
     int return_from_function;
 
-    while (true)
-    {
+    while (true) {
         ptr_read = (char *)malloc(sizeof(char) * (READ_SIZE + ONE));
-        if (NULL == ptr_read)
-        {
+        if (NULL == ptr_read) {
             PUT_ERROR(strerror(errno));
             return FUNCTION_FAILURE;
         }
         readline = read(fd, ptr_read, READ_SIZE);
-        if (FUNCTION_FAILURE == readline)
-        {
+        if (FUNCTION_FAILURE == readline) {
             PUT_ERROR(strerror(errno));
             free(ptr_read);
             return FUNCTION_FAILURE;
         }
-        else
-        {
+        else {
             ptr_read[readline] = NIL;
         }
         return_from_function = CONTAINS_CHAR(ptr_read, EOL, return_from_function);
-        if (FUNCTION_SUCCESS == return_from_function)
-        {
+        if (FUNCTION_SUCCESS == return_from_function) {
             return_from_function = manage_buffer(ptr_read, ptr_container, ptr_return);
             free(ptr_read);
             if (FUNCTION_FAILURE == return_from_function)
-            {
                 return FUNCTION_FAILURE;
-            }
             else
-            {
                 break;
-            }
         }
-        else
-        {
+        else {
             return_from_function = join(ptr_return, *ptr_container);
-            if (FUNCTION_FAILURE == return_from_function)
-            {
+            if (FUNCTION_FAILURE == return_from_function) {
                 free(ptr_read);
                 return FUNCTION_FAILURE;
             }
-            else
-            {
+            else {
                 free(*ptr_container);
                 *ptr_container = NULL;
             }
             return_from_function = join(ptr_return, ptr_read);
             if (FUNCTION_FAILURE == return_from_function)
-            {
                 return FUNCTION_FAILURE;
-            }
             free_args_va_arguments(1, ptr_read);
             if (READ_ZERO == readline)
-            {
                 return EXIT_EOF;
-            }
             else if (READ_SIZE == readline)
-            {
                 continue;
-            }
             else
-            {
                 break;
-            }
         }
     }
     return FUNCTION_SUCCESS;
@@ -244,26 +203,21 @@ char *get_next_line(const int __fd, const int __mode)
     int return_from_function;
 
     return_from_function = read_line_into_buffer(__fd, &ptr_container, &ptr_return);
-    if (FUNCTION_FAILURE == return_from_function)
-    {
+    if (FUNCTION_FAILURE == return_from_function) {
         free_args_va_arguments(TWO, ptr_container, ptr_return);
         return NULL;
     }
-    if (EXIT_EOF == return_from_function)
-    {
+    if (EXIT_EOF == return_from_function) {
         free(ptr_container);
-        if (strlen(ptr_return) != ZERO)
-        {
+        if (strlen(ptr_return) != ZERO) {
             return ptr_return;
         }
-        else
-        {
+        else {
             free(ptr_return);
             return NULL;
         }
     }
-    if (SIMPLE_MODE == __mode)
-    {
+    if (SIMPLE_MODE == __mode) {
         free(ptr_container);
         ptr_container = NULL;
     }
